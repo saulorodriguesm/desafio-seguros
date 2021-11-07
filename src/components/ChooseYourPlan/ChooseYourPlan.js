a44c694aa2ce284c853a29b02612ce6019649c54
saulorodriguesm <saulorodriguesm@gmail.com>
2021-11-07 20:57:58 -0300

    json response added

--- src/components/ChooseYourPlan/ChooseYourPlan.js
+++ src/components/ChooseYourPlan/ChooseYourPlan.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Plan1 from "../../assets/plan1.png";
 import Plan2 from "../../assets/plan2.png";
 import Plan3 from "../../assets/plan3.png";
 import Plan4 from "../../assets/plan4.png";
 
 const ChooseYourPlan = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [plan, setPlan] = useState("");
+  const [offer, setOffer] = useState();
+  const [isOfferOpen, setOfferOpen] = useState(false);
+  const offerName = offer?.nome;
+
+  const getData = () => {
+    fetch("/get_response_ofertas_residencial.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(async (data) => {
+        setOffer(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="sectionContainer">
       <div className="sectionTitle">
@@ -13,7 +35,12 @@ const ChooseYourPlan = () => {
       </div>
       <div className="separator" />
       <div className="plans">
-        <div className="planCard">
+        <button
+          onClick={() => {
+            setOfferOpen(true);
+          }}
+          className="planCard"
+        >
           {" "}
           <img
             src={Plan1}
@@ -22,8 +49,14 @@ const ChooseYourPlan = () => {
             height="207"
           />{" "}
           <span> seguro residencial </span>
-        </div>
-        <div className="planCard">
+        </button>
+        <button
+          onClick={() => {
+            setOpen(true);
+            setPlan("seguro auto");
+          }}
+          className="planCard"
+        >
           {" "}
           <img
             src={Plan2}
@@ -32,8 +65,14 @@ const ChooseYourPlan = () => {
             height="207"
           />
           <span> seguro auto </span>
-        </div>
-        <div className="planCard">
+        </button>
+        <button
+          onClick={() => {
+            setOpen(true);
+            setPlan("odonto");
+          }}
+          className="planCard"
+        >
           {" "}
           <img
             src={Plan3}
@@ -42,8 +81,14 @@ const ChooseYourPlan = () => {
             height="207"
           />
           <span> plano odonto </span>
-        </div>
-        <div className="planCard">
+        </button>
+        <button
+          onClick={() => {
+            setOpen(true);
+            setPlan("assistência bem-estar");
+          }}
+          className="planCard"
+        >
           {" "}
           <img
             src={Plan4}
@@ -52,7 +97,22 @@ const ChooseYourPlan = () => {
             height="207"
           />
           <span> assistência bem-estar </span>
-        </div>
+        </button>
+      </div>
+      <div className={isOpen || isOfferOpen ? "overlay" : ""}> </div>
+      <div className={isOpen ? "oopsModal" : "invisible"}>
+        <button onClick={() => setOpen(false)}> X </button> <span>oops :(</span>
+        parece que o plano {plan} ainda não teve a sua modal desenvolvida.
+        Enquanto isso, clique no card "Seguro Residencial" e confira todas as
+        opções de seguro residencial do Itaú.
+      </div>
+      <div className={isOfferOpen ? "modalSegurosResidencial" : "invisible"}>
+        <button onClick={() => setOfferOpen(false)}> X </button>
+        <span>
+          {" "}
+          Confira as coberturas do plano Seguro Residencial da{" "}
+          <b> {offerName}:</b>{" "}
+        </span>
       </div>
     </div>
   );
